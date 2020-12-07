c3c6b2bd33479767c025ebdac4564856a2b1f0a0
Amanda Edgar <plainjane916@gmail.com>
2020-12-07 15:13:50 -0600

    changed/added some css styling

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Favorite Images';
+  image1 = 'https://i.guim.co.uk/img/media/b26621268d472eebb28ac7fca8a1ca4186f45473/0_122_2672_1603/master/2672.jpg?width=700&quality=85&auto=format&fit=max&s=e10f2976241364cef69b1efaa7f58275';
+  image2 = 'https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F150929101049-black-coffee-stock.jpg';
+  image3 = 'https://img.jakpost.net/c/2019/03/02/2019_03_02_66706_1551461528._large.jpg';
 
   constructor() { }
 
